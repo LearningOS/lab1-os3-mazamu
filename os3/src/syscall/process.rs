19becaa1c7a88139973e2c94c36f6c7d1d7967fc
mazamu <506143016@qq.com>
2023-03-16 08:11:58 +0000

    duoshi

---
+++ os3/src/syscall/process.rs
@@ -0,0 +1,17 @@
+
+use crate::config::{MAX_APP_NUM, MAX_SYSCALL_NUM};
+use crate::task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus};
+
+pub fn sys_exit(exit_code: i32) -> ! {
+    // info!("[kernel] Application exited with code {}", exit_code);
+    // run_next_app()
+    println!("[kernel] Application exited with code {}", exit_code);
+    exit_current_and_run_next();
+    panic!("Unreachable in sys_exit!");
+}
+
+pub fn sys_yield() -> isize {
+    suspend_current_and_run_next();
+    0
+}
+
